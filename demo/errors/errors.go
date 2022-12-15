104588b79fb9b1d7bed374928a9b60c3628834fe
Sabry Awad <dr.sabry1997@gmail.com>
2022-12-15 14:15:44 +0200

    errors demo

---
+++ demo/errors/errors.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Stuff struct {
+	values []int
+}
+
+func (s *Stuff) Get(index int) (int, error) {
+	if index > len(s.values) {
+		return 0, fmt.Errorf("no element at index %v", index)
+	}
+	return s.values[index], nil
+}
+
+func main() {
+	stuff := Stuff{}
+	value, err := stuff.Get(1)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("value is", value)
+	}
+}
