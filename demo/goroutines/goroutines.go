f7090207efb68147e009918a56b908dc82d677be
Sabry Awad <dr.sabry1997@gmail.com>
2022-12-16 03:52:35 +0200

    goroutines demo

---
+++ demo/goroutines/goroutines.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"time"
+	"unicode"
+)
+
+func main() {
+	data := []rune{'a', 'b', 'c', 'd'}
+	var capitalized []rune
+
+	capIt := func(r rune) {
+		capitalized = append(capitalized, unicode.ToUpper(r))
+		fmt.Printf("%c done!\n", r)
+	}
+
+	fmt.Printf("Before: %c\n", capitalized)
+	for i := 0; i < len(data); i++ {
+		go capIt(data[i])
+	}
+	time.Sleep(100 * time.Millisecond)
+	fmt.Printf("After: %c\n", capitalized)
+}
