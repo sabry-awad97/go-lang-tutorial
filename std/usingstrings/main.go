67cad47c801ca9789ef279288790ac3997acac8c
Sabry Awad <dr.sabry1997@gmail.com>
2022-12-18 05:14:58 +0200

    String Scan

--- std/usingstrings/main.go
+++ std/usingstrings/main.go
@@ -97,9 +97,24 @@ func scanNewLine() {
 	}
 }
 
+func sourceScan() {
+	var name string
+	var category string
+	var price float64
+	source := "Lifejacket Watersports 48.95"
+	n, err := fmt.Sscan(source, &name, &category, &price)
+	if err == nil {
+		Printfln("Scanned %v values", n)
+		Printfln("Name: %v, Category: %v, Price: %.2f", name, category, price)
+	} else {
+		Printfln("Error: %v", err.Error())
+	}
+}
+
 func main() {
 	// formatting()
 	// scan()
 	// scanSlice()
-	scanNewLine()
+	// scanNewLine()
+	sourceScan()
 }
