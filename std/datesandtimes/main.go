f1a78aa2cbfc4c4e62f0845a51f47509e0dd9fe0
Sabry Awad <dr.sabry1997@gmail.com>
2022-12-18 11:48:41 +0200

    Formatting Time Values

--- std/datesandtimes/main.go
+++ std/datesandtimes/main.go
@@ -1,11 +1,14 @@
 package main
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
-func PrintTime(label string, t *time.Time) {
-	Printfln("%s: Day: %v: Month: %v Year: %v",
-		label, t.Day(), t.Month(), t.Year())
-}
+// func PrintTime(label string, t *time.Time) {
+// 	Printfln("%s: Day: %v: Month: %v Year: %v",
+// 		label, t.Day(), t.Month(), t.Year())
+// }
 
 func representDateTime() {
 	current := time.Now()
@@ -16,6 +19,22 @@ func representDateTime() {
 	PrintTime("UNIX", &unix)
 }
 
+func PrintTime(label string, t *time.Time) {
+	layout := "Day: 02 Month: Jan Year: 2006"
+	fmt.Println(label, t.Format(layout))
+	// fmt.Println(label, t.Format(time.RFC822Z))
+}
+
+func FormattingTimeValues() {
+	current := time.Now()
+	specific := time.Date(1995, time.June, 9, 0, 0, 0, 0, time.Local)
+	unix := time.Unix(1433228090, 0)
+	PrintTime("Current", &current)
+	PrintTime("Specific", &specific)
+	PrintTime("UNIX", &unix)
+}
+
 func main() {
-	representDateTime()
+	// representDateTime()
+	FormattingTimeValues()
 }
