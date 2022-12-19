3604dba1d5e1c9ecd98e6fcbd3d402df10fa4738
Sabry Awad <dr.sabry1997@gmail.com>
2022-12-19 11:50:32 +0200

    Comparing Time Values

--- std/datesandtimes/main.go
+++ std/datesandtimes/main.go
@@ -99,11 +99,19 @@ func manipulateTime() {
 	}
 }
 
+func CompareTimeValues() {
+	t1, _ := time.Parse(time.RFC822Z, "09 Jun 95 04:59 +0100")
+	t2, _ := time.Parse(time.RFC822Z, "08 Jun 95 23:59 -0400")
+	Printfln("Equal Method: %v", t1.Equal(t2))
+	Printfln("Equality Operator: %v", t1 == t2)
+}
+
 func main() {
 	// representDateTime()
 	// FormattingTimeValues()
 	// parsingDateString()
 	// parseLocation()
 	// specifyTimeZone()
-	manipulateTime()
+	// manipulateTime()
+	CompareTimeValues()
 }
