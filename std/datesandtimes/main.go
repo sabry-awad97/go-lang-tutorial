057db421fd1614ae0fc9379a30213829938a1f97
Sabry Awad <dr.sabry1997@gmail.com>
2022-12-19 11:57:18 +0200

    Representing Durations

--- std/datesandtimes/main.go
+++ std/datesandtimes/main.go
@@ -106,6 +106,20 @@ func CompareTimeValues() {
 	Printfln("Equality Operator: %v", t1 == t2)
 }
 
+func duration() {
+	var d time.Duration = time.Hour + (30 * time.Minute)
+	Printfln("Hours: %v", d.Hours())
+	Printfln("Mins: %v", d.Minutes())
+	Printfln("Seconds: %v", d.Seconds())
+	Printfln("Millseconds: %v", d.Milliseconds())
+	rounded := d.Round(time.Hour)
+	Printfln("Rounded Hours: %v", rounded.Hours())
+	Printfln("Rounded Mins: %v", rounded.Minutes())
+	trunc := d.Truncate(time.Hour)
+	Printfln("Truncated Hours: %v", trunc.Hours())
+	Printfln("Rounded Mins: %v", trunc.Minutes())
+}
+
 func main() {
 	// representDateTime()
 	// FormattingTimeValues()
@@ -113,5 +127,6 @@ func main() {
 	// parseLocation()
 	// specifyTimeZone()
 	// manipulateTime()
-	CompareTimeValues()
+	// CompareTimeValues()
+	duration()
 }
