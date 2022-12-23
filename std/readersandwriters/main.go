6a58dd3d55fcdbd4cd5304672d4899d7f15def66
Sabry Awad <dr.sabry1997@gmail.com>
2022-12-23 06:31:55 +0200

    Writer interface

--- std/readersandwriters/main.go
+++ std/readersandwriters/main.go
@@ -25,6 +25,30 @@ func understandingReaders() {
 	processData(r)
 }
 
-func main() {
+func understandingWriters() {
+	processData := func(reader io.Reader, writer io.Writer) {
+		var buf = make([]byte, 2)
 
+		for {
+			count, err := reader.Read(buf)
+			if count > 0 {
+				writer.Write(buf[0:count])
+				Printfln("Read %v bytes: %v", count, string(buf[0:count]))
+			}
+
+			if err == io.EOF {
+				break
+			}
+		}
+	}
+
+	r := strings.NewReader("Kayak")
+	var builder strings.Builder
+	processData(r, &builder)
+	Printfln("String builder contents: %s", builder.String())
+}
+
+func main() {
+	// understandingReaders()
+	understandingWriters()
 }
