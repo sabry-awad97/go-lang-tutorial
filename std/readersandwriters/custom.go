25c266410eac747ca961b8fefb82c19addf30f8b
Sabry Awad <dr.sabry1997@gmail.com>
2022-12-23 11:19:28 +0200

    Unbuffered Writes

--- std/readersandwriters/custom.go
+++ std/readersandwriters/custom.go
@@ -20,3 +20,27 @@ func (cr *CustomReader) Read(slice []byte) (count int, err error) {
 	}
 	return
 }
+
+type CustomWriter struct {
+	writer     io.Writer
+	writeCount int
+}
+
+func NewCustomWriter(writer io.Writer) *CustomWriter {
+	return &CustomWriter{writer, 0}
+}
+
+func (cw *CustomWriter) Write(slice []byte) (count int, err error) {
+	count, err = cw.writer.Write(slice)
+	cw.writeCount++
+	Printfln("Custom Writer: %v bytes", count)
+	return
+}
+
+func (cw *CustomWriter) Close() (err error) {
+	if closer, ok := cw.writer.(io.Closer); ok {
+		closer.Close()
+	}
+	Printfln("Total Writes: %v", cw.writeCount)
+	return
+}
--- std/readersandwriters/main.go
+++ std/readersandwriters/main.go
@@ -133,6 +133,22 @@ func readerWrapper() {
 	Printfln("Read data: %v", writer.String())
 }
 
+func unbufferedWrites() {
+	text := "It was a boat. A small boat."
+	var builder strings.Builder
+	var writer = NewCustomWriter(&builder)
+	for i := 0; true; {
+		end := i + 5
+		if end >= len(text) {
+			writer.Write([]byte(text[i:]))
+			break
+		}
+		writer.Write([]byte(text[i:end]))
+		i = end
+	}
+	Printfln("Written data: %v", builder.String())
+}
+
 func main() {
 	// understandingReaders()
 	// understandingWriters()
@@ -142,5 +158,6 @@ func main() {
 	// multipleWriters()
 	// EchoData()
 	// limitData()
-	readerWrapper()
+	// readerWrapper()
+	unbufferedWrites()
 }
