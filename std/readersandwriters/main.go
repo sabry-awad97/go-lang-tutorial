5b21f12f18ba55831dd8d33ff69c91aad65d49cf
Sabry Awad <dr.sabry1997@gmail.com>
2022-12-23 07:18:14 +0200

    Echoing Data

--- std/readersandwriters/main.go
+++ std/readersandwriters/main.go
@@ -91,11 +91,23 @@ func multipleWriters() {
 	Printfln("Writer #3: %v", w3.String())
 }
 
+func EchoData() {
+	r1 := strings.NewReader("Kayak")
+	r2 := strings.NewReader("Lifejacket")
+	r3 := strings.NewReader("Canoe")
+	concatReader := io.MultiReader(r1, r2, r3)
+	var writer strings.Builder
+	teeReader := io.TeeReader(concatReader, &writer)
+	ConsumeData(teeReader)
+	Printfln("Echo data: %v", writer.String())
+}
+
 func main() {
 	// understandingReaders()
 	// understandingWriters()
 	// copyData()
 	// pipe()
 	// multipleReader()
-	multipleWriters()
+	// multipleWriters()
+	EchoData()
 }
