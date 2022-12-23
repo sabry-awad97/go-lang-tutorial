6ca89a8077f1ce78e751d4662cfcf2c117ffd9d1
Sabry Awad <dr.sabry1997@gmail.com>
2022-12-23 07:23:25 +0200

    Limiting Data

--- std/readersandwriters/main.go
+++ std/readersandwriters/main.go
@@ -102,6 +102,15 @@ func EchoData() {
 	Printfln("Echo data: %v", writer.String())
 }
 
+func limitData() {
+	r1 := strings.NewReader("Kayak")
+	r2 := strings.NewReader("Lifejacket")
+	r3 := strings.NewReader("Canoe")
+	concatReader := io.MultiReader(r1, r2, r3)
+	limited := io.LimitReader(concatReader, 5)
+	ConsumeData(limited)
+}
+
 func main() {
 	// understandingReaders()
 	// understandingWriters()
@@ -109,5 +118,6 @@ func main() {
 	// pipe()
 	// multipleReader()
 	// multipleWriters()
-	EchoData()
+	// EchoData()
+	limitData()
 }
