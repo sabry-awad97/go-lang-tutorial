82e49933e7d415c21ebbc28b9d095046c7cf807b
Sabry Awad <dr.sabry1997@gmail.com>
2022-12-23 06:51:22 +0200

    Copy Data

--- std/readersandwriters/main.go
+++ std/readersandwriters/main.go
@@ -48,7 +48,24 @@ func understandingWriters() {
 	Printfln("String builder contents: %s", builder.String())
 }
 
+func copyData() {
+	processData := func(reader io.Reader, writer io.Writer) {
+		count, err := io.Copy(writer, reader)
+		if err == nil {
+			Printfln("Read %v bytes", count)
+		} else {
+			Printfln("Error: %v", err.Error())
+		}
+	}
+
+	r := strings.NewReader("Kayak")
+	var builder strings.Builder
+	processData(r, &builder)
+	Printfln("String builder contents: %s", builder.String())
+}
+
 func main() {
 	// understandingReaders()
-	understandingWriters()
+	// understandingWriters()
+	copyData()
 }
